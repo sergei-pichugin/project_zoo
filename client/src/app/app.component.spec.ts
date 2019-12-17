8173ff6839835f5246bd4ebb836170ee90f42632
Sergei Pichugin <spichugin@inbox.ru>
2019-12-17 10:45:14 +0300

    Angular and Material versions updates to 8.2.

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('client');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to client!');
+    expect(compiled.querySelector('.content span').textContent).toContain('client app is running!');
   });
 });
